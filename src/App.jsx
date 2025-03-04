2e0175dd0d8051502c799867af58f84716274123
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-03-04 18:35:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import Signup from "./signup";
-import Login from "./login";
-import Navbar from "./components/navbar"; // Assuming you have a Navbar component
-import Home from "./pages/home";
+import React, { Component } from 'react'
 
-function App() {
-  const location = useLocation();
-  const nonavbarpath = ["/signup", "/"];
-
-  return (
-    <div>
-      {/* Conditionally render Navbar */}
-      {!nonavbarpath.includes(location.pathname) && <Navbar />}
-
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </div>
-  );
-}
-
-// ✅ Fix: Add basename for GitHub Pages
-export default function Root() {
-  return (
-    <Router basename="/quiz-platform-react-Re-Engineering">
-      <App />
-    </Router>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
